433884be56447989f40244e85f1b70bfa2aca270
edz <>
2020-04-02 16:48:36 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  base: '/note/',
+  dest: './dist',
+  title: '前端学习笔记',
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'CSS',
+        children: ['csssecret/aaa']
+      },
+    ]
+  }
+  
+}
