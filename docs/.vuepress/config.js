ecccf80415aeb0a26840c4b9ec88f1b4197d3123
yqwoshuai <358805791@qq.com>
2020-04-04 10:54:55 +0800

    文章迁移

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  base: "/note/",
-  dest: "./dist",
-  title: "前端学习笔记",
-  themeConfig: {
-    sidebar: [
-      {
-        title: "《JavaScript高级程序设计》",
-        children: [
-          "js-develop/1",
-          "js-develop/2",
-          "js-develop/3",
-          "js-develop/4",
-          "js-develop/5"
+    base: "/note/",
+    dest: "./dist",
+    title: "前端学习笔记",
+    themeConfig: {
+        sidebar: [{
+                title: "《JavaScript高级程序设计》",
+                children: [
+                    "js-develop/1",
+                    "js-develop/2",
+                    "js-develop/3",
+                    "js-develop/4",
+                    "js-develop/5"
+                ]
+            },
+            {
+                title: "《JavaScript忍者秘籍》",
+                children: [
+                    "js-nijia/1"
+                ]
+            },
+            {
+                title: "各类知识点系列",
+                children: [
+                    "other-know/1",
+                    "other-know/2",
+                    "other-know/3"
+                ]
+            }
         ]
-      },
-      {
-        title: "《JavaScript忍者秘籍》",
-        children: [
-          "js-nijia/1"
-        ]
-      },
-      {
-        title: "各类知识点系列",
-        children: [
-          "other-know/1",
-          "other-know/2"
-        ]
-      }
-    ]
-  }
+    }
 };
