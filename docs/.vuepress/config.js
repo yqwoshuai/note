833c0a8cb4c1011aeb020b360d8f46fe04890a81
edz <>
2020-04-14 21:00:08 +0800

    文章迁移

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,42 @@
 module.exports = {
-    base: "/note/",
-    dest: "./dist",
-    title: "前端学习笔记",
-    themeConfig: {
-        sidebar: [{
-                title: "《JavaScript高级程序设计》",
-                children: [
-                    "js-develop/1",
-                    "js-develop/2",
-                    "js-develop/3",
-                    "js-develop/4",
-                    "js-develop/5"
-                ]
-            },
-            {
-                title: "《JavaScript忍者秘籍》",
-                children: [
-                    "js-nijia/1"
-                ]
-            },
-            {
-                title: "《JavaScript数据结构与算法》",
-                children: [
-                    "js-algorithms/1",
-                    "js-algorithms/2"
-                ]
-            },
-            {
-                title: "《JavaScript设计模式》",
-                children: [
-                    "js-design/1",
-                    "js-design/2"
-                ]
-            },
-            {
-                title: "各类知识点系列",
-                children: [
-                    "other-know/1",
-                    "other-know/2",
-                    "other-know/3"
-                ]
-            }
+  base: "/note/",
+  dest: "./dist",
+  title: "前端学习笔记",
+  themeConfig: {
+    sidebar: [
+      {
+        title: "《JavaScript高级程序设计》",
+        children: [
+          "js-develop/1",
+          "js-develop/2",
+          "js-develop/3",
+          "js-develop/4",
+          "js-develop/5"
         ]
-    }
+      },
+      {
+        title: "《JavaScript忍者秘籍》",
+        children: [
+          "js-nijia/1",
+          "js-nijia/2",
+          "js-nijia/3",
+          "js-nijia/4",
+          "js-nijia/5",
+          "js-nijia/6"
+        ]
+      },
+      {
+        title: "《JavaScript数据结构与算法》",
+        children: ["js-algorithms/1", "js-algorithms/2"]
+      },
+      {
+        title: "《JavaScript设计模式》",
+        children: ["js-design/1", "js-design/2"]
+      },
+      {
+        title: "各类知识点系列",
+        children: ["other-know/1", "other-know/2", "other-know/3"]
+      }
+    ]
+  }
 };
